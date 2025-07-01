979d1bbe13ffb1a80f76cf221f2c250e24dc79f3
Denis Anfimov <denis.anfimov@netcracker.com>
2025-07-01 15:26:13 +0500

    CPCAP-890 support maven repositories

--- renovate-config-cli/src/main/java/org/qubership/cloud/actions/renovate/RenovateConfigToJsConverter.java
+++ renovate-config-cli/src/main/java/org/qubership/cloud/actions/renovate/RenovateConfigToJsConverter.java
@@ -1,7 +1,6 @@
 package org.qubership.cloud.actions.renovate;
 
 import org.qubership.cloud.actions.renovate.model.RenovateConfig;
-import org.qubership.cloud.actions.renovate.model.RenovateMaven;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,26 +21,23 @@ public class RenovateConfigToJsConverter {
                         String.join(",\n", value.stream().map(s -> tab.repeat(2) + "'" + s + "'").toList()),
                         tab)
                 ));
-        Optional.ofNullable(config.getMaven()).map(RenovateMaven::getRepositories).ifPresent(value -> sb.append(tab)
-                .append(String.format("maven: {\n%s\n%s},\n",
-                        String.format("%srepositories: [\n%s\n%s]",
-                                tab.repeat(2),
-                                String.join(",\n", value.stream().map(s -> {
-                                    StringBuilder sb1 = new StringBuilder(tab.repeat(3) + "{\n");
-                                    sb1.append(tab.repeat(4)).append("id: ").append("'").append(s.getId()).append("'").append("\n");
-                                    sb1.append(tab.repeat(4)).append("url: ").append("'").append(s.getUrl()).append("'").append("\n");
-                                    if (s.getUsername() != null) {
-                                        sb1.append(tab.repeat(4)).append("username: ").append("'").append(s.getUsername()).append("'");
-                                        if (s.getPassword() != null) sb1.append("\n");
-                                    }
-                                    if (s.getPassword() != null) {
-                                        sb1.append(tab.repeat(4)).append("password: ").append("'").append(s.getPassword()).append("'");
-                                    }
-                                    sb1.append("\n").append(tab.repeat(3)).append("}");
-                                    return sb1.toString();
-                                }).toList()),
-                                tab.repeat(2)),
-                        tab)
+        Optional.ofNullable(config.getHostRules()).ifPresent(value -> sb.append(tab)
+                .append(String.format("hostRules: [\n%s\n%s]",
+                        String.join(",\n", value.stream().map(s -> {
+                            StringBuilder sb1 = new StringBuilder(tab.repeat(3) + "{\n");
+                            sb1.append(tab.repeat(4)).append("hostType: ").append("'").append(s.getHostType()).append("'").append("\n");
+                            sb1.append(tab.repeat(4)).append("matchHost: ").append("'").append(s.getMatchHost()).append("'").append("\n");
+                            if (s.getUsername() != null) {
+                                sb1.append(tab.repeat(4)).append("username: ").append("'").append(s.getUsername()).append("'");
+                                if (s.getPassword() != null) sb1.append("\n");
+                            }
+                            if (s.getPassword() != null) {
+                                sb1.append(tab.repeat(4)).append("password: ").append("'").append(s.getPassword()).append("'");
+                            }
+                            sb1.append("\n").append(tab.repeat(3)).append("}");
+                            return sb1.toString();
+                        }).toList()),
+                        tab.repeat(2))
                 ));
         Optional.ofNullable(config.getPackageRules()).ifPresent(value -> sb.append(tab)
                 .append(String.format("packageRules: [%s\n%s],",
--- renovate-config-cli/src/main/java/org/qubership/cloud/actions/renovate/RenovateMavenRepositoryConverter.java
+++ renovate-config-cli/src/main/java/org/qubership/cloud/actions/renovate/RenovateMavenRepositoryConverter.java
@@ -1,18 +1,15 @@
 package org.qubership.cloud.actions.renovate;
 
-import org.qubership.cloud.actions.renovate.model.RenovateMavenRepository;
+import org.qubership.cloud.actions.renovate.model.RenovateHostRule;
 import picocli.CommandLine;
 
-import java.util.Arrays;
-import java.util.List;
-
-public class RenovateMavenRepositoryConverter implements CommandLine.ITypeConverter<List<RenovateMavenRepository>> {
+public class RenovateMavenRepositoryConverter implements CommandLine.ITypeConverter<RenovateHostRule> {
 
     @Override
-    public List<RenovateMavenRepository> convert(String value) throws CommandLine.TypeConversionException {
+    public RenovateHostRule convert(String value) throws CommandLine.TypeConversionException {
         if (value == null || value.isBlank()) return null;
         try {
-            return Arrays.stream(value.split(",")).map(RenovateMavenRepository::new).toList();
+            return new RenovateHostRule(value);
         } catch (IllegalArgumentException e) {
             throw new CommandLine.TypeConversionException(e.getMessage());
         }
