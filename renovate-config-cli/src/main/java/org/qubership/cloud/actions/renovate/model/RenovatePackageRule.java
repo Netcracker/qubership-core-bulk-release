b1feb0092dd753cd0d7f171fb7525addfc6d9d74
Denis Anfimov <denis.anfimov@netcracker.com>
2025-07-22 20:13:41 +0800

    CPCAP-559 enable default maven rule

--- renovate-config-cli/src/main/java/org/qubership/cloud/actions/renovate/model/RenovatePackageRule.java
+++ renovate-config-cli/src/main/java/org/qubership/cloud/actions/renovate/model/RenovatePackageRule.java
@@ -2,12 +2,52 @@ package org.qubership.cloud.actions.renovate.model;
 
 import lombok.Data;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Data
 public class RenovatePackageRule {
+
+    public static Pattern pattern = Pattern.compile("^\\[(matchManagers=(?<matchManagers>.+?);?)?" +
+                                                    "(matchDatasources=(?<matchDatasources>.+?);?)?" +
+                                                    "(matchPackageNames=(?<matchPackageNames>.+?);?)?" +
+                                                    "(matchUpdateTypes=(?<matchUpdateTypes>.+?);?)?" +
+                                                    "(registryUrls=(?<registryUrls>.+?);?)?" +
+                                                    "(allowedVersions=(?<allowedVersions>.+?);?)?" +
+                                                    "(groupName=(?<groupName>.+?);?)?" +
+                                                    "(automerge=(?<automerge>.+?);?)?" +
+                                                    "]$");
+    List<String> matchManagers;
     List<String> matchDatasources;
     List<String> matchPackageNames;
+    List<String> matchUpdateTypes;
     List<String> registryUrls;
     String allowedVersions;
+    String groupName;
+    Boolean automerge;
+
+    public RenovatePackageRule() {
+    }
+
+    public RenovatePackageRule(String value) {
+        Matcher matcher = pattern.matcher(value);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(String.format("Invalid package rule: '%s'. Must match: '%s'", value, pattern));
+        }
+        this.matchManagers = toList(matcher, "matchManagers");
+        this.matchDatasources = toList(matcher, "matchDatasources");
+        this.matchPackageNames = toList(matcher, "matchPackageNames");
+        this.matchUpdateTypes = toList(matcher, "matchUpdateTypes");
+        this.registryUrls = toList(matcher, "registryUrls");
+        this.allowedVersions = matcher.group("allowedVersions");
+        this.groupName = matcher.group("groupName");
+        this.automerge = Boolean.parseBoolean(matcher.group("automerge"));
+    }
+
+    static List<String> toList(Matcher matcher, String group) {
+        return Arrays.stream(Optional.ofNullable(matcher.group(group)).map(v -> v.split("&")).orElse(new String[0])).toList();
+    }
 }
