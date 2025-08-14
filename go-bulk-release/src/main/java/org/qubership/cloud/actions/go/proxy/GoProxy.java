beee5f4ec9601431a37fedb26ee863c837298ddc
larkin <larkin.qubership@gmail.com>
2025-08-14 11:17:40 +0400

    fix: correct GOPROXY path

--- go-bulk-release/src/main/java/org/qubership/cloud/actions/go/proxy/GoProxy.java
+++ go-bulk-release/src/main/java/org/qubership/cloud/actions/go/proxy/GoProxy.java
@@ -4,15 +4,30 @@ import org.qubership.cloud.actions.go.util.CommandRunner;
 
 public class GoProxy {
     public static void enableGoProxy() {
-        String[][] commands = {
-                //todo vlla change to real one
-                {"go", "env", "-w", "GOPROXY=file:///Z:/home/user/bulk_release/GOPROXY,https://proxy.golang.org,direct"},
-                {"go", "env", "-w", "GONOPROXY="},
-                {"go", "env", "-w", "GONOSUMDB=github.com/netcracker/*,github.com/taurmorchant/*"},
-                {"go", "env", "-w", "GOPRIVATE="}
-        };
-        for (String[] cmd : commands) {
-            CommandRunner.runCommand(cmd);
+        //todo vlla just fo local dev
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("win")) {
+            String[][] commands = {
+                    //todo vlla change to real one
+                    {"go", "env", "-w", "GOPROXY=file:///Z:/home/user/bulk_release/GOPROXY,https://proxy.golang.org,direct"},
+                    {"go", "env", "-w", "GONOPROXY="},
+                    {"go", "env", "-w", "GONOSUMDB=github.com/netcracker/*,github.com/taurmorchant/*"},
+                    {"go", "env", "-w", "GOPRIVATE="}
+            };
+            for (String[] cmd : commands) {
+                CommandRunner.runCommand(cmd);
+            }
+        }
+        else {
+            String[][] commands = {
+                    {"go", "env", "-w", "GOPROXY=file:///tmp/GOPROXY,https://proxy.golang.org,direct"},
+                    {"go", "env", "-w", "GONOPROXY="},
+                    {"go", "env", "-w", "GONOSUMDB=github.com/netcracker/*,github.com/taurmorchant/*"},
+                    {"go", "env", "-w", "GOPRIVATE="}
+            };
+            for (String[] cmd : commands) {
+                CommandRunner.runCommand(cmd);
+            }
         }
     }
 }
