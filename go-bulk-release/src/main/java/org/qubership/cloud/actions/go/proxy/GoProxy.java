e6ff0e271b088fe0a5864cadec7f7a4925786794
larkin <larkin.qubership@gmail.com>
2025-08-15 20:05:22 +0400

    fix: goproxy

--- go-bulk-release/src/main/java/org/qubership/cloud/actions/go/proxy/GoProxy.java
+++ go-bulk-release/src/main/java/org/qubership/cloud/actions/go/proxy/GoProxy.java
@@ -26,7 +26,7 @@ public class GoProxy {
             String[][] commands = {
                     {"go", "env", "-w", "GOPROXY=file:///tmp/GOPROXY,https://proxy.golang.org,direct"},
                     {"go", "env", "-w", "GONOPROXY="},
-                    {"go", "env", "-w", "GONOSUMDB=github.com/netcracker/*,github.com/taurmorchant/*"},
+                    {"go", "env", "-w", "GONOSUMDB=github.com/netcracker/*,github.com/taurmorchant/*,github.com/vlla-test-organization/*"},
                     {"go", "env", "-w", "GOPRIVATE="}
             };
             for (String[] cmd : commands) {
