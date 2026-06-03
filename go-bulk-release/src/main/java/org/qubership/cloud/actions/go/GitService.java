548a4c61a13dbb684ca7ce7d05e67c90b6dbe0c8
Vladislav Larkin <larkin.qubership@gmail.com>
2026-06-03 16:23:56 +0400

    fix: check git push status

--- go-bulk-release/src/main/java/org/qubership/cloud/actions/go/GitService.java
+++ go-bulk-release/src/main/java/org/qubership/cloud/actions/go/GitService.java
@@ -6,7 +6,9 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.RefNotFoundException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.transport.PushResult;
 import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.transport.RemoteRefUpdate;
 import org.eclipse.jgit.transport.TagOpt;
 import org.qubership.cloud.actions.go.model.GitConfig;
 import org.qubership.cloud.actions.go.model.UnexpectedException;
@@ -134,11 +136,14 @@ public class GitService {
 
     public void pushChanges(File repository) {
         try (Git git = Git.open(repository)) {
-            git.push()
+            Iterable<PushResult> results = git.push()
                     .setCredentialsProvider(config.getCredentialsProvider())
                     .setPushAll()
                     .setPushTags()
                     .call();
+            checkPushResults(results, repository.getAbsolutePath());
+        } catch (UnexpectedException e) {
+            throw e;
         } catch (Exception e) {
             throw new UnexpectedException(e);
         }
@@ -147,14 +152,30 @@ public class GitService {
 
     public void pushBranch(File repository, String branchName) {
         try (Git git = Git.open(repository)) {
-            git.push()
+            Iterable<PushResult> results = git.push()
                     .setCredentialsProvider(config.getCredentialsProvider())
                     .setRefSpecs(new RefSpec(branchName + ":" + branchName))
                     .call();
+            checkPushResults(results, repository.getAbsolutePath());
+        } catch (UnexpectedException e) {
+            throw e;
         } catch (Exception e) {
             throw new UnexpectedException(e);
         }
         log.info("Pushed branch '{}' to git: repo: {}", branchName, repository.getAbsoluteFile());
+    }
+
+    private void checkPushResults(Iterable<PushResult> results, String repositoryPath) {
+        for (PushResult result : results) {
+            for (RemoteRefUpdate update : result.getRemoteUpdates()) {
+                RemoteRefUpdate.Status status = update.getStatus();
+                if (status != RemoteRefUpdate.Status.OK && status != RemoteRefUpdate.Status.UP_TO_DATE) {
+                    throw new UnexpectedException(
+                            "Push to repository '%s' was rejected by remote. Status: %s, message: %s"
+                                    .formatted(repositoryPath, status, update.getMessage()));
+                }
+            }
+        }
     }
 
     private List<String> getDiff(Git git, DiffEntry.ChangeType changeType) throws GitAPIException {
